f87a135b23b955415f18c4a31aafe2d5aca096cc
Mateusz Bugala <mateusz.bugala@gmail.com>
2019-04-05 10:55:21 +0200

    changed Security access

--- src/main/java/pl/coderslab/config/SecurityConfig.java
+++ src/main/java/pl/coderslab/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,9 +18,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/users/add", "/login").permitAll()
+                .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .antMatchers("/users/all").hasAuthority("ROLE_ADMIN")
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and().csrf().disable().formLogin().loginPage("/login")
                 .usernameParameter("email")
                 .passwordParameter("password")
